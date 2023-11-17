e7bf294c5d311ce84d45767cdafc09fb10a6c8e2
matt <matt-hope@hotmail.com>
2023-11-17 15:50:36 -0500

    add menu changes to main

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ class StockTrade {
 // portfolio class will store a vector of StockTrade objects
 class Portfolio {
 	public: 
-		vector<StockTrade> myTrades;
+		vector<StockTrade> trades;
 		// TODO: add tradeIDCounter to portfolio class
 		// to keep track of trade id numbers
 
@@ -34,25 +34,62 @@ class Portfolio {
 			cout << "Portfolio constructor" << endl;
 		}
 
-		void addTrade(StockTrade trade) {
-			myTrades.push_back(trade);
+		void addTradeNoValidate(StockTrade trade) {
+			trades.push_back(trade);
+		}
+
+		void addTrade() {
+			// Declare variables for user input
+			int tID;
+			string symbol;
+			string type;
+			int numOfShares;
+			double stockValue;
+
+			// Get user input
+			cout << "Enter trade ID: ";
+			cin >> tID;
+			cout << "Enter stock symbol (3-4 characters): ";
+			cin >> symbol;
+			cout << "Enter trade type (buy/sell): ";
+			cin >> type;
+			cout << "Enter quantity of shares: ";
+			cin >> numOfShares;
+			cout << "Enter price per share: ";
+			cin >> stockValue;
+
+			bool tradeValid = validateTradeInfo(tID, symbol, type, numOfShares, stockValue);
+
+			if (tradeValid) {
+				StockTrade trade(tID, symbol, type, numOfShares, stockValue);
+				trades.push_back(trade);
+			} else {
+				cout << "Trade not added." << endl;
+			}
 		}
 
 		void printTrades() {
-			for (int i = 0; i < myTrades.size(); i++) {
-				cout << myTrades[i].tradeID  << " ";
-				cout << myTrades[i].tradeType << " ";
-				cout << myTrades[i].stockSymbol<< " ";
-				cout << myTrades[i].quantity << " ";
-				cout << myTrades[i].price << " " << endl;
+			cout << "\n+----------------------------------+\n";
+			cout << "|          List of Trades          |\n";
+			cout << "+----------------------------------+\n";
+			for (StockTrade trade: trades) {
+				cout << trade.tradeID  << " ";
+				cout << trade.tradeType << " ";
+				cout << trade.stockSymbol<< " ";
+				cout << trade.quantity << " ";
+				cout << trade.price << " " << endl;
 			}
+			cout << "+----------------------------------+\n";
 		}
 
 		// to calculate the quantity of each stock in the portfolio
 		// we can use a map, iterate through all the trades and add the quantity to the map with the stock symbol as the key
 		map<string,int> calculateQtyShares() {
+			cout << "\n+----------------------------------+\n";
+			cout << "|    Quantity of Shares per Stock  |\n";
+			cout << "+----------------------------------+\n";
 			map<string, int> stockQty;
-			for (StockTrade trade: myTrades) {
+			for (StockTrade trade: trades) {
 				// add quantity if the trade is a buy
 				// subtract quantity if the trade is a sell 
 				if (trade.tradeType == "buy") {
@@ -65,13 +102,17 @@ class Portfolio {
 				cout << entry.first << " " << entry.second << " shares" << endl;
 			}
 			return stockQty;
+			cout << "+----------------------------------+\n";
 		}
 
 		void calculatePortfolioValue() {
+			cout << "\n\n+----------------------------------+\n";
+			cout << "|      Current Portfolio Value     |\n";
+			cout << "+----------------------------------+\n";
 			map<string, int> stockShares;       // To store the total number of shares for each stock.
 			map<string, double> stockValue;     // To store the total value of each stock.
 
-			for (StockTrade& trade : myTrades) {
+			for (StockTrade& trade : trades) {
 				if (trade.tradeType == "buy") {
 					stockShares[trade.stockSymbol] += trade.quantity;
 					stockValue[trade.stockSymbol] += trade.quantity * trade.price;
@@ -103,22 +144,27 @@ bool validateTradeInfo(int tradeId, string stockSymbol, string tradeType, int qu
 	bool tradeValid = true;
 
 	if (stockSymbol.length() < 3 || stockSymbol.length() > 4) {
+		cout << "Error: Stock symbol should be 3-4 characters long." << endl;
 		tradeValid = false;
 	}
 
 	if (tradeType != "buy" || tradeType != "sell") {
+		cout << "Error: Trade type should be either 'buy' or 'sell'." << endl;
 		tradeValid = false;
 	}
 
 	if (quantity < 0) {
+		cout << "Error: Quantity should be a positive integer." << endl;
 		tradeValid = false;
 	}
 
 	if (price < 0) {
+		cout << "Error: Price should be a positive number." << endl;
 		tradeValid = false;
 	}
 	
 	if (tradeId < 0) {
+		cout << "Error: TradeID should be a positive integer." << endl;
 		tradeValid = false;
 	}
 
@@ -128,26 +174,66 @@ bool validateTradeInfo(int tradeId, string stockSymbol, string tradeType, int qu
 // TODO: refer to workshopW9/IntBinaryTree.h and workshopW9/main.cpp to get an example of how to separate the class definition from the main program/ make header file
 
 int main () { 
-	cout << "main program" << endl;
-	StockTrade myTrade(1, "AAPL", "buy", 100, 100.00);
-	StockTrade mySellTrade(2, "AAPL", "Sell", 100, 100.00);
-	StockTrade myTrade2(3, "GOOG", "buy", 115, 175.00);
-	StockTrade myTrade3(4, "GOOG", "buy", 222, 175.00);
 
 	Portfolio myPortfolio;
+	int choice = 0;
 
-	myPortfolio.addTrade(myTrade);
-	myPortfolio.addTrade(mySellTrade);
-	myPortfolio.addTrade(myTrade);
-	myPortfolio.addTrade(myTrade);
-	myPortfolio.addTrade(myTrade2);
-	myPortfolio.addTrade(myTrade3);
+	while (true) {
+		cout << "====================================\n";
+		cout << "= Stock Portfolio Management System =\n";
+		cout << "====================================\n";
 
-	myPortfolio.printTrades();
+		cout << "1. Add a Trade\n";
+		cout << "2. Print Trades\n";
+		cout << "3. Calculate Quantity of Shares\n";
+		cout << "4. Calculate Portfolio Value\n";
+		cout << "5. Exit\n";
+		cout << "Enter your choice: ";
+		cin >> choice;
 
-	myPortfolio.calculateQtyShares();
+		switch (choice) {
+		case 1:
+			myPortfolio.addTrade();
+			break;
+		case 2:
+			myPortfolio.printTrades();
+			break;
+		case 3:
+			myPortfolio.calculateQtyShares();
+			break;
+		case 4:
+			myPortfolio.calculatePortfolioValue();
+			break;
+		case 5:
+			cout << "Exiting program." << endl;
+			return 0;
+		default:
+			cout << "Invalid choice. Please try again." << endl;
+		}
+	}
 
-	myPortfolio.calculatePortfolioValue();
+	// testing main
+	// cout << "main program" << endl;
+	// StockTrade myTrade(1, "AAPL", "buy", 100, 100.00);
+	// StockTrade mySellTrade(2, "AAPL", "Sell", 100, 100.00);
+	// StockTrade myTrade2(3, "GOOG", "buy", 115, 175.00);
+	// StockTrade myTrade3(4, "GOOG", "buy", 222, 175.00);
+
+	// Portfolio myPortfolio;
+
+	// myPortfolio.addTradeNoValidate(myTrade);
+	// myPortfolio.addTradeNoValidate(mySellTrade);
+	// myPortfolio.addTradeNoValidate(myTrade);
+	// myPortfolio.addTradeNoValidate(myTrade);
+	// myPortfolio.addTradeNoValidate(myTrade2);
+	// myPortfolio.addTradeNoValidate(myTrade3);
+
+	// myPortfolio.printTrades();
+
+	// myPortfolio.calculateQtyShares();
+
+	// myPortfolio.calculatePortfolioValue();
+	// end testing main
 
 	return 0; 
 }
